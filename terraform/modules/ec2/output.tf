9a39cc41dee3f6724beb3dd71d7df18db1afe720
ekansh789 <ekansh9644@gmail.com>
2026-05-19 10:37:36 +0530

    Day 11 learning helm readme will update soon

--- terraform/modules/ec2/output.tf
+++
@@ -1,3 +0,0 @@
-output "public_ip" {
-  value = aws_instance.ekansh-ec2bootcamp.public_ip
-}
--- terraform/output.tf
+++
@@ -1,7 +0,0 @@
-output "instance_public_ip" {
-  value = module.ec2.public_ip
-}
-
-output "ssh_command" {
-  value = "ssh -i Ekansh-bootcamp.pem ec2-user@${module.ec2.public_ip}"
-}
